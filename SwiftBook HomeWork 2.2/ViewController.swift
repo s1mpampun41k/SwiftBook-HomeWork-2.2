e06beb13bd7e3474613661fdfe25bf9894fb74cc
s1mpampun41k <132660824+s1mpampun41k@users.noreply.github.com>
2025-03-13 03:29:41 +0300

    Added logic for changing traffic lights by pressing a button.

--- SwiftBook HomeWork 2.2/ViewController.swift
+++ SwiftBook HomeWork 2.2/ViewController.swift
@@ -1,11 +1,55 @@
 import UIKit
 
+enum CurrentLights {
+    case red, yellow, green
+}
 final class ViewController: UIViewController {
-
+    
+    @IBOutlet var redTrafficLightView: UIView!
+    @IBOutlet var yellowTrafficLightView: UIView!
+    @IBOutlet var greenTrafficLightView: UIView!
+    
+    @IBOutlet var changeTrafficLightStatusButton: UIButton!
+    
+    private var currentLight = CurrentLights.red
+    
+    override func viewDidLayoutSubviews() {
+        redTrafficLightView.layer.cornerRadius = redTrafficLightView.frame.width / 2
+        yellowTrafficLightView.layer.cornerRadius = yellowTrafficLightView.frame.width / 2
+        greenTrafficLightView.layer.cornerRadius = greenTrafficLightView.frame.width / 2
+    }
+    
     override func viewDidLoad() {
+        changeTrafficLightStatusButton.layer.cornerRadius = 10
+        
+        redTrafficLightView.alpha = 0.3
+        yellowTrafficLightView.alpha = 0.3
+        greenTrafficLightView.alpha = 0.3
+        
         super.viewDidLoad()
     }
 
+    @IBAction func changeTrafficState() {
+        if changeTrafficLightStatusButton.currentTitle == "START"{
+            changeTrafficLightStatusButton.setTitle("NEXT", for: .normal)
+        }
 
+        switch currentLight {
+        case .red:
+            greenTrafficLightView.alpha = 0.3
+            redTrafficLightView.alpha = 1
+            currentLight = CurrentLights.yellow
+        case .yellow:
+            redTrafficLightView.alpha = 0.3
+            yellowTrafficLightView.alpha = 1
+            currentLight = CurrentLights.green
+        default:
+            yellowTrafficLightView.alpha = 0.3
+            greenTrafficLightView.alpha = 1
+            currentLight = CurrentLights.red
+        }
+        
+    }
+    
 }
 
